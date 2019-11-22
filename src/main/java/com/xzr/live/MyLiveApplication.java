126cbdf67ecc8bd5c2cba15a17b5ec05f6796487
xzr <1477415198@qq.com>
2019-11-22 15:39:50 +0800

    第一次提交

---
+++ src/main/java/com/xzr/live/MyLiveApplication.java
@@ -0,0 +1,16 @@
+package com.xzr.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author :xzr
+ * @date :2019-11-22
+ * @description :启动类
+ */
+@SpringBootApplication
+public class MyLiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyLiveApplication.class);
+    }
+}
