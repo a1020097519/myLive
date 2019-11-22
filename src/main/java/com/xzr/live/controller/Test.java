055a04a5e48ee7457699127aa9d55043c29fa5e1
xzr <1477415198@qq.com>
2019-11-22 19:31:33 +0800

    停止脚本

--- src/main/java/com/xzr/live/controller/Test.java
+++ src/main/java/com/xzr/live/controller/Test.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-    @GetMapping("/hi")
+    @GetMapping("/hi1")
     public String sayHi(){
-        return "hi";
+        return "hi1";
     }
 }
--- stop.sh
+++ stop.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+echo "---------------- stoping server -------------------"
+
+pid=`ps -ef | grep $1 | grep -v grep | awk '{print $2}'`
+
+if [ -n "$pid" ];then
+  echo "kill -9 pid:"$pid
+  kill -9 $pid
+fi
